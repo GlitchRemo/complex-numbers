84e7eabd95ed535e02e058f8ef2324ae077c8e5a
Riya <GlitchRemo@users.noreply.github.com>
2023-05-02 18:45:47 +0530

    Created scafolding

---
+++ test/main-test.js
@@ -0,0 +1,8 @@
+const {describe, it} = require('node:test');
+const {strictEqual} = require('assert');
+
+describe('Complex Numbers', function() {
+  it('should get the real part of a complex number', function() {
+    strictEqual(1, 1);
+  });
+});
