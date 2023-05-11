a9d3b3cc065789c5bf9a747ee44c30665dc85407
Riya <GlitchRemo@users.noreply.github.com>
2023-05-11 10:38:06 +0530

    Refactored Test Cases of Imaginary Numbers

--- test/imaginary-number-test.js
+++ test/imaginary-number-test.js
@@ -1,9 +1,8 @@
 const { describe, it } = require('node:test');
-const { ok } = require('assert');
+const { ok, strictEqual } = require('assert');
 const { Imaginary } = require('../src/imaginary-number.js');
 const { Real } = require('../src/real-number.js');
 
-const minusOne = new Real(-1);
 const zero = new Real(0);
 const one = new Real(1);
 const two = new Real(2);
@@ -12,12 +11,14 @@ const three = new Real(3);
 describe('Imaginary Number', function () {
   describe('equality', function () {
 
-    it('should be reflective', function () {
-      const actual = new Imaginary(two);
+    it('should be reflexive', function () {
+      const x = new Imaginary(two);
+      const y = new Imaginary(two);
 
-      const expected = new Imaginary(two);
+      const actual = x.isEqual(y);
+      const expected = true;
 
-      ok(actual.isEqual(expected));
+      strictEqual(actual, expected);
     });
   });
 
@@ -27,31 +28,31 @@ describe('Imaginary Number', function () {
       const addend = new Imaginary(two);
       const augend = new Imaginary(one);
 
-      const expected = new Imaginary(three);
-      
       const actual = addend.add(augend);
-  
+      const expected = new Imaginary(three);
+        
       ok(actual.isEqual(expected));
     });
 
     it('should be commutative', function () {
-      const addend = new Imaginary(one);
-      const augend = new Imaginary(two);
+      const a = new Imaginary(one);
+      const b = new Imaginary(two);
 
-      const expected = new Imaginary(three);
+      const x = a.add(b);
+      const y = b.add(a);
 
-      const actual = addend.add(augend);
+      const actual = x.isEqual(y);
+      const expected = true;
 
-      ok(actual.isEqual(expected));
+      strictEqual(actual, expected);
     });
 
     it('should provide the original back on identity', function () {
       const number = new Imaginary(two);
       const identity = new Imaginary(zero);
 
-      const expected = number;
-  
       const actual = number.add(identity);
+      const expected = number;
 
       ok(actual.isEqual(expected));
     });  
@@ -63,31 +64,31 @@ describe('Imaginary Number', function () {
       const subtrahend = new Imaginary(two);
       const minuend = new Imaginary(one);
 
-      const expected = new Imaginary(one);
-      
       const actual = subtrahend.subtract(minuend);
+      const expected = new Imaginary(one);
   
       ok(actual.isEqual(expected));
     });
 
     it('should not be commutative', function () {
-      const subtrahend = new Imaginary(one);
-      const minuend = new Imaginary(two);
+      const a = new Imaginary(one);
+      const b = new Imaginary(two);
 
-      const expected = new Imaginary(minusOne);
+      const x = a.subtract(b);
+      const y = b.subtract(a);
 
-      const actual = subtrahend.subtract(minuend);
+      const actual = x.isEqual(y);
+      const expected = false;
 
-      ok(actual.isEqual(expected));
+      strictEqual(actual, expected);
     });
 
     it('should provide the original back on identity', function () {
       const number = new Imaginary(two);
       const identity = new Imaginary(zero);
 
-      const expected = number;
-  
       const actual = number.subtract(identity);
+      const expected = number;
 
       ok(actual.isEqual(expected));
     });  
@@ -99,32 +100,32 @@ describe('Imaginary Number', function () {
       const multiplicand = new Imaginary(two);
       const multiplier = new Imaginary(one);
 
-      const expected = new Real(-2);
-      
       const actual = multiplicand.multiply(multiplier);
+      const expected = new Real(-2);
   
       ok(actual.isEqual(expected));
     });
 
     it('should be commutative', function () {
-      const multiplicand = new Imaginary(one);
-      const multiplier = new Imaginary(two);
+      const a = new Imaginary(one);
+      const b = new Imaginary(two);
+      
+      const x = b.multiply(a);
+      const y = a.multiply(b);
 
-      const expected = new Real(-2);
+      const actual = x.isEqual(y);
+      const expected = true;
 
-      const actual = multiplicand.multiply(multiplier);
-
-      ok(actual.isEqual(expected));
+      strictEqual(actual, expected);
     });
 
     it('should provide the negation of original back on identity', function () {
       const number = new Imaginary(two);
       const identity = new Imaginary(one);
 
+      const actual = number.multiply(identity);
       const expected = new Real(-2);
   
-      const actual = number.multiply(identity);
-
       ok(actual.isEqual(expected));
     });  
   });
